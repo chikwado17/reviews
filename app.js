3b1db0c44e933ddcbda28bb596a655455296e02e
chikwado17 <chikwadonworie@gmail.com>
2020-12-21 11:26:25 +0100

    completed the review project

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ const author = document.getElementById("author");
 const job = document.getElementById('job');
 const info = document.getElementById("info");
 
-const prevBtn = document.querySelector('prev-btn');
+const prevBtn = document.querySelector('.prev-btn');
 const nextBtn = document.querySelector('.next-btn');
 const randomBtn = document.querySelector(".random-btn");
 
@@ -54,6 +54,41 @@ let currentItem = 0;
 
 //load initial item
 window.addEventListener("DOMContentLoaded", () => {
-  const item = reviews[currentItem];
-  img.src = item.img;
+    showPerson(currentItem);
+});
+
+//show person based on item
+function showPerson(person) {
+    const item = reviews[person];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+}
+
+
+//show next person
+nextBtn.addEventListener('click', () => {
+  currentItem++;
+  if(currentItem > reviews.length - 1) {
+    currentItem = 0;
+  }
+  showPerson(currentItem);
+});
+
+
+prevBtn.addEventListener('click', () => {
+  currentItem--;
+  if(currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+  showPerson(currentItem);
+});
+
+
+//show random person
+randomBtn.addEventListener('click', () => {
+  const randomItems = Math.floor(Math.random() * reviews.length);
+  currentItem = randomItems;
+  showPerson(currentItem);
 });
